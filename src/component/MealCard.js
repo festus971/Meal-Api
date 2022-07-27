275fc4b21d765fadecffc4cb8374d816a702b198
+ <+>
2022-07-27 14:18:48 +0300

    shifting data

--- src/component/MealCard.js
+++ src/component/MealCard.js
@@ -2,14 +2,20 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-export default function MealCard({mealName, mealThumbnail, mealId}){
+export default function MealCard({mealName, mealThumbnail, mealId,onDetailsClick,meal}){
+
+    function handleOnClick(){
+        onDetailsClick(meal)
+
+    }
+
 
     return (
         <div className="card" style={{width: 18 + 'rem'}}>
             <img src={mealThumbnail} className="card-img-top" alt={mealName}/>
             <div className="card-body">
                 <h5 className="card-title">{mealName}</h5>
-                <Link to={"/meal/"+mealId} className="btn btn-primary">DETAILS</Link>
+                <Link to={"/meal/"+mealId}onClick={handleOnClick} className="btn btn-primary">DETAILS</Link>
             </div>
         </div>
     )
