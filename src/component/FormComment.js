20703a969bb093705e884bd20804d5b8e51036b9
+ <+>
2022-07-28 23:13:24 +0300

    adding the Post METHOD

--- src/component/FormComment.js
+++ src/component/FormComment.js
@@ -13,6 +13,23 @@ const handleformsuggestions=(event)=>{
     setValues({...values,Suggestions:event.target.value})
 }
 
+// fetch("https://stormy-gorge-09324.herokuapp.com/meals/",{
+//           method: "POST",
+//           headers:{
+//             "Content-Type": "application/json",
+//           },
+//           body:JSON.stringify,
+//         })
+//         .then(res => res.json())
+//         .then(values=>console.log(values)
+            
+//         )
+      
+
+
+
+
+
     return(
         <div>
             <input value={values} placeholder="reviews" name="reviews" onChange={handleformreviews}></input>
