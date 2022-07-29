d4d780322935efd9824eab63da618c736a6ed43b
+ <+>
2022-07-29 22:07:38 +0300

    welcome words

---
+++ src/component/Welcome.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./css/Login.css";
+
+const Welcome = () => {
+  return (
+    <div className='images'>
+      <p>Feeling Hungry ?
+        <br/>
+        Well look no further, we've got you
+        <br/>
+        Try out our amazing meals ranging from different categories of foods Just Click on the Home icon
+        <br/>
+        Welcome!
+        </p>
+    </div>
+  )
+}
+
+export default Welcome;
