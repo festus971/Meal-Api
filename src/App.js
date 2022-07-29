0c6b99b90c23cb75be3487484c7d503e3a97330f
+ <+>
2022-07-29 22:06:43 +0300

    shiting the file

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PropagateLoader } from "react-spinners";
+import NavBar from "../src/component/NavBar";
+import Home from "../src/component/Home";
+import Login from "../src/component/Login";
+import Details from "../src/component/Details";
+import Welcome from "../src/component/Welcome";
+
+function App() {
+  const [meal, setMeal] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
+  function onDetailsClick(clickedMeal) {
+    setMeal(clickedMeal);
+  }
+
+  return (
+    <div>
+      <Router>
+        <div>
+          <NavBar />
+        </div>
+        <Routes>
+          <Route exact path="/" element={<Home onDetailsClick={onDetailsClick} />}></Route>
+          <Route exact path="/meal/:mealId" element={<Details meal={meal} />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/images" element={<Welcome />}></Route>
+        </Routes>
+      </Router>
+
+      {loading ? <PropagateLoader loading={loading} size={10} /> : <div></div>}
+    </div>
+  );
+}
+
+export default App;
