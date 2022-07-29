2ca6560b2e2db4e2a31bbd08d9049f38383f3ba9
+ <+>
2022-07-29 14:22:20 +0300

    changes on form

--- src/component/FormComment.js
+++ src/component/FormComment.js
@@ -2,28 +2,16 @@ import React,{useState} from "react";
 
 function FormComments(){
 const[values,setValues]=useState({
-    reviews:"" 
-})
+    reviews:""})
 
 const handleformreviews=(event)=>{
     setValues({...values,reviews:event.target.value})
 }
 
 
-// fetch("https://stormy-gorge-09324.herokuapp.com/meals/",{
-//           method: "POST",
-//           headers:{
-//             "Content-Type": "application/json",
-//           },
-//           body:JSON.stringify,
-//         })
-//         .then(res => res.json())
-//         .then(values=>console.log(values)
-            
-//         )
     return(
         <div>
-            <input type="text" value={values} name="comment." placeholder="enter your reviews" onChange={handleformreviews}/>
+           
         </div>
     )
 }
